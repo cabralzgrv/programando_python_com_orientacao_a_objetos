7be50893983fd9680aa85ce9dd39b3607c52f2c2
Lucas Cabral <cabralz18@outlook.com>
2023-09-29 06:11:06 -0300

    Melhorando o código

--- forca.py
+++ forca.py
@@ -1,9 +1,56 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    arquivo = open("palavra.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta: str = palavras[numero].lower()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+
+        chute: str = input("Qual letra?")
+        chute = chute.strip().lower()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index = index + 1
+            else:
+                erros += 1
+                print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu :(")
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
